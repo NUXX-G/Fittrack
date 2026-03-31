9b0ce24ecb61f3b41f9f31f53a631523a3d5c9db
NUXX-G <nelsonffkarlsson@gmail.com>
2026-03-31 21:53:16 +0200

    feat: crear entidad User

--- src/main/java/com/gymnos/fittrack/model/User.java
+++ src/main/java/com/gymnos/fittrack/model/User.java
@@ -1,4 +1,44 @@
 package com.gymnos.fittrack.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private LocalDate birthDate;
+
+    private String gender;
+
+    private Double weight;
+
+    private Double height;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role
+    {
+        USER, ADMIN
+    }
 }
