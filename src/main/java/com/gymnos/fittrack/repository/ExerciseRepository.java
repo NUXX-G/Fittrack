a4b1930b5b6b5d832bfb9d1920ca869816d824b1
Petter5-5 <yeraytrejosanchez@gmail.com>
2026-04-14 10:08:55 +0200

    feat: crear ExerciseRepository

--- src/main/java/com/gymnos/fittrack/repository/ExerciseRepository.java
+++ src/main/java/com/gymnos/fittrack/repository/ExerciseRepository.java
@@ -1,5 +1,17 @@
 package com.gymnos.fittrack.repository;
 
-public class ExerciseRepository {
+import com.gymnos.fittrack.model.Exercise;
+import com.gymnos.fittrack.model.MuscleGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
+import java.util.List;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<Exercise,Long>
+{
+    Optional<Exercise> findByName(String name);
+    List<Exercise> findByMuscleGroup(MuscleGroup m);
 }
